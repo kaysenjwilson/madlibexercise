99cfcecbd61f77fbb955ddd5cc0246064bf0f6bc
Kaysen Wilson <kaysenjwilson@gmail.com>
2024-06-18 15:49:19 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+adjective = input("add an adjective")
+verb = input("add a verb")
+plural_noun = input("add a plural noun")
+verb_ing = input("add a verb ending in -ing")
+noun = input("add a noun")
+
+print(f"Learning Python is {adjective}. It helps you {verb} {plural_noun} and {verb} {plural_noun}. Python is the best for {verb_ing} and {verb_ing}. Start your {noun} now!")
+
+
+
+
+
+
